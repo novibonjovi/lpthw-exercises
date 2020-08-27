4a6ea76da14f6d8989c3d5ec8e9c835d2abbd79e
novibonjovi <novak.nikolic92@gmail.com>
2020-08-27 22:52:51 +0200

    Added new exercises

--- ex03.1.py
+++ ex03.1.py
@@ -1,4 +1,4 @@
-print("My stocks are worth 9'223 Swiss Francs at the moment")
-print("I made a profit of 1'473 Swiss Francs")
+print("My stocks are worth 9'22 Swiss Francs at the moment")
+print("I made a profit of 1'47 Swiss Francs")
 
-print("That's a profit of", (100 / (9223 - 1473) * 9223) - 100, "percent")
+print("That's a profit of", (100 / (922 - 147) * 922) - 100, "percent")
